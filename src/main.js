170a9852541567b2921cec6af7b1f3ae7641ec17
11-ZS <1029856983@qq.com>
2019-06-19 05:47:46 +0800

    interface init

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 import './assets/init.css'
 
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
 
 Vue.config.productionTip = false
+
+Vue.use(elementUI)
 
 new Vue({
   router,
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,66 +1,73 @@
 <template>
     <div class="body">
         <div class="container">
-            <d-tabs defaultOpen="1" ref="tabs">
-                <d-pane label="综合" flag="1">
-                    <d-row>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-                        <d-col :span="3">ds</d-col>
-
-                    </d-row>
-                </d-pane>
-                <d-pane label="其他" flag="2">2</d-pane>
-                <button slot="other" @click="addThingsToDo" class="label">
-                    +
-                </button>
-
-
-                <d-pane label="其他" flag="3">3</d-pane>
-            </d-tabs>
+            <el-tabs v-model="activeName" type="border-card">
+                <el-tab-pane label="事情" name="thing">
+                    <el-row :gutter="20">
+                        <el-col :span="5" v-for="(el,index) in things" :key="index" >
+                            <el-popover
+                                placement="top-start"
+                                :title="el.name"
+                                width="200"
+                                trigger="hover"
+                                :content="el.description"
+                            >
+                                <div slot="reference" class="thing" @click="handle">
+                                    <div>{{el.name}}</div>
+                                    <div>{{getTime(el.startTime)}}</div>
+                                </div>
+                            </el-popover>
+                        </el-col>
+                    </el-row>
+                </el-tab-pane>
+                <el-tab-pane label="已完成" name="daily"></el-tab-pane>
+            </el-tabs>
         </div>
     </div>
 </template>
 
 <script>
-import dTabs from "../components/d-tabs.vue";
-import dPane from "../components/d-pane.vue";
-import dRow from "../components/d-row.vue";
-import dCol from "../components/d-col.vue";
-
 export default {
-    name: "HelloWorld",
-    components: {
-        dTabs,
-        dPane,
-        dRow,
-        dCol
-    },
-    props: {
-        msg: String
-    },
     data() {
         return {
-            tabs: {}
+            activeName: "thing",
+            things: [
+                {
+                    name: "练习Vue",
+                    description: "tabs and pane",
+                    startTime: new Date(),
+                    endTime: new Date(),
+                    isOver: false
+                },
+                {
+                    name: "练习Vue",
+                    description: "tabs and pane",
+                    startTime: new Date(),
+                    endTime: new Date(),
+                    isOver: false
+                }
+            ]
         };
     },
     methods: {
-        clickk() {
-            console.log("click....");
+        getTime(time) {
+            return (
+                time.getFullYear() +
+                "-" +
+                (time.getMonth() + 1) +
+                "-" +
+                time.getDate() +
+                " " +
+                time.getHours() +
+                ":" +
+                time.getMinutes() +
+                ":" +
+                time.getSeconds()
+            );
         },
-        addThingsToDo() {
-            console.log(this.tabs.currentFocus);
-            this.tabs.labelList.push({ label: "new", flag: 6 });
+        handle() {
+            console.log("a");
         }
-    },
-    mounted() {
-        this.tabs = this.$refs.tabs;
     }
 };
 </script>
@@ -75,31 +82,34 @@ html {
     width: 100%;
     height: 100%;
     border: 1px solid blue;
+    box-sizing: border-box;
 }
 .container {
     position: absolute;
-    width: 100%;
+    width: 950px;
+    height: 65%;
     margin: 0px auto;
     top: 50%;
     left: 50%;
     border: 1px solid red;
     transform: translate(-50%, -50%);
+    box-sizing: border-box;
+    padding: 10px;
 }
 
-.content {
-    height: 70vh;
+.thing {
+    border: 1px solid rgb(187, 187, 187);
+    padding: 10px;
+    box-sizing: border-box;
+    border-radius: 5px;
+
+    webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
 }
-.labels {
-    margin-top: 10px;
-    margin-left: 10px;
-}
-.addButton {
-    position: relative;
-    padding: 0;
-    width: 30px;
-    border: 0px solid #d7dde4;
-    border-radius: 3px;
-    margin-top: 0px;
-    cursor: pointer;
+.thing:hover {
+    background: rgb(213, 243, 255);
+    border-color: rgb(119, 203, 206);
 }
 </style>
